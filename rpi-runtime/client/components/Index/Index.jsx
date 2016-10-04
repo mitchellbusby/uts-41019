69ff69983619b7bb2375b9bc046fd0ccac49071e
Mitchell Busby <mitchbusby@gmail.com>
2016-10-04 12:23:13 +1100

    Placeholders for rooms now up

--- rpi-runtime/client/components/Index/Index.jsx
+++ rpi-runtime/client/components/Index/Index.jsx
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 
+import RoomComponent from '../Room/Room';
+
 class IndexComponent extends Component {
   render() {
-    if (this.props.items.length === 0) {
+    let { AvailableRooms } = this.props;
+
+    if (AvailableRooms.length === 0) {
       return (
-        <p ref="empty">Index is empty.</p>
+        <section className={'index-row'}>
+          No rooms available that match your criteria...
+        </section>
       );
     }
 
     return (
       <section>
-        <h2>react-webpack-boilerplate</h2>
-        <ul ref="indexList" className="index-list">
-          {this.props.items.map((item, index) => {
-            return (<li key={index}>item {item}</li>);
-          })}
-        </ul>
+          <ul className={'rooms'}>
+            { AvailableRooms.map((item,index) => {
+              return <RoomComponent />
+            })}
+          </ul>
+          <div className={'indicator'}>
+          </div>
       </section>
     );
   }
 }
 
+/*IndexComponent.props = {
+
+}*/
+
 IndexComponent.defaultProps = {
-  items: []
+  items: [],
+  DisplayIsOn: true,
+  AvailableRooms: [{}, {}, {}],
 };
 
 export default IndexComponent;
---
+++ rpi-runtime/client/components/Room/Room.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class RoomComponent extends Component {
+  render() {
+    let { RoomName, PeopleCount, TimeFree, Temperature, Rank } = this.props;
+
+    // TODO: better logic for this conversion
+    let timeFreePrettyPrint =  `${ Math.floor(TimeFree / 60) } hours free`;
+
+    return (
+        <li className={'roomComponent'}>
+          <h3>{ RoomName }</h3>
+          <p>
+            <span>{ PeopleCount } people&nbsp;|&nbsp;</span>
+            <span>{timeFreePrettyPrint} hours free&nbsp;|&nbsp;</span>
+            <span>{ Temperature }</span>
+          </p>
+          <p>{ Rank }</p>
+        </li>
+      )
+  }
+}
+
+RoomComponent.defaultProps = {
+  // In seconds
+  TimeFree: 200,
+  RoomName: 'CB11.06.11',
+  Rank: 'Best',
+  Temperature: '22C',
+  PeopleCount: 2
+}
+
+export default RoomComponent;
