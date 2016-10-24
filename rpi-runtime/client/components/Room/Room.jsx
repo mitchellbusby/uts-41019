1f75cac4b4d00e715f6790e3c6d14dff9247dfba
Mitchell Busby <mitchbusby@gmail.com>
2016-10-24 15:56:36 +1100

    Adds toggling of rooms

--- rpi-runtime/client/components/Room/Room.jsx
+++ rpi-runtime/client/components/Room/Room.jsx
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
 
 class RoomComponent extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      RoomFormat: 0 
+    }
+
+  }
+
   render() {
     let { RoomName, PeopleCount, TimeFree, Temperature } = this.props;
 
-    // TODO: better logic for this conversion
+    let { RoomFormat } = this.state;
+
+    // Room format 0 for building style, format 1 for 
+
+    let roomNameFormatted;
+
+    if (RoomFormat == 0) {
+      roomNameFormatted = RoomName;
+    }
+    else {
+
+      let roomData = RoomName.split('.');
+
+      roomNameFormatted = `Level ${roomData[1]}, Room ${roomData[2]}`; 
+    }
+    
     let timeFreePrettyPrint =  `free until ${ TimeFree }`;
 
     let peopleText = PeopleCount > 1 ? 'people' : 'person';
 
     return (
-        <li className={'roomComponent'}>
-          <h2>{ RoomName }</h2>
+        <li className={'roomComponent'} onClick={this.handleClick.bind(this)}>
+          <h2>{ roomNameFormatted }</h2>
           <p>
-            <span>{ PeopleCount } {peopleText} there currently&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-            <span>{timeFreePrettyPrint}&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+            <span>{ PeopleCount } { peopleText } there currently&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+            <span>{ timeFreePrettyPrint }&nbsp;&nbsp;|&nbsp;&nbsp;</span>
             <span>{ Temperature }°C</span>
           </p>
             <hr />
         </li>
       )
+  }
+
+  handleClick() {
+    
+    let newRoomFormat;
+
+    if (this.state.RoomFormat == 0) {
+      newRoomFormat = 1;
+    }
+    else {
+      newRoomFormat = 0;
+    }
+
+    this.setState({
+      RoomFormat: newRoomFormat
+    });
+
   }
 }
 
